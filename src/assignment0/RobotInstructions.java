e495f18313291e2356ab97836c207440a18df0d1
griffinlitherland <griffinlitherland@localhost>
2025-01-22 11:50:57 -0600

    I drew a tree with the robot

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,75 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.forward(150);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(120);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(40);
+		robot.turnRight(90);
+		robot.forward(140);
+		robot.turnLeft(180);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(150);
+		robot.turnLeft(90);
+		robot.forward(500);
+		robot.turnRight(180);
+		robot.forward(1000);
+		
+		
 	}
 
 }
